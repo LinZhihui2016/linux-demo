37cf02ea88303ba51ba729a7f973867081880023
叶子 <519403248@qq.com>
2021-04-16 16:46:49 +0800

    Hello World

--- routes/index.ts
+++ routes/index.ts
@@ -4,5 +4,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/hello', (req, res) => {
+  res.render('hello', { title: 'Hello World' });
+});
 export default router;
