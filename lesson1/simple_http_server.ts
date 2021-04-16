a063f2d8ecaa64f5c2a6bfc8970fa2b48ac33123
叶子 <519403248@qq.com>
2021-04-16 16:14:04 +0800

    lesson1结束

--- lesson1/simple_http_server.ts
+++ lesson1/simple_http_server.ts
@@ -1,10 +1,9 @@
-import http from 'http'
+import * as  http from 'http'
 
 const port = 8808;
 const server = http.createServer()
 server.on('request', (req, res) => {
   res.statusCode = 200
-
   res.end(JSON.stringify({ url: req.url, ip: req.ip }))
 })
 
