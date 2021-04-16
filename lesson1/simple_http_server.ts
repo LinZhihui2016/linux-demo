6131d335bdf228e6b9ac1db921ff8b36f0a60f9b
叶子 <519403248@qq.com>
2021-04-16 15:19:05 +0800

    server base

--- lesson1/simple_http_server.ts
+++ lesson1/simple_http_server.ts
@@ -1,3 +1,15 @@
 import http from 'http'
+
+const port = 8808;
 const server = http.createServer()
-server.listen(8808)
+server.on('request', (req, res) => {
+  res.statusCode = 200
+
+  res.end(JSON.stringify({ url: req.url, ip: req.ip }))
+})
+
+server.on('listening', () => {
+  console.log(`start ${ port }`)
+})
+
+server.listen(port)
