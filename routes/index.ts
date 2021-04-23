0ded082357fbd3d3fbebb9a8a04c003e88ac3290
叶子 <519403248@qq.com>
2021-04-23 16:06:50 +0800

    跨域

--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import { User } from "../moodels/user";
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -9,11 +8,4 @@ router.get('/hello', (req, res) => {
   res.render('hello', { title: 'Hello World' });
 });
 
-router.get('/user', (req, res) => {
-  const firstName = req.$get.str('firstName', 'Hello')
-  const lastName = req.$get.str('lastName', 'World')
-  const age = req.$get.num('age', 12)
-  const user = User.add(firstName, lastName, +age)
-  res.render('user', { user, allUser: User.getAllName.join(' ; ') })
-})
 export default router;
