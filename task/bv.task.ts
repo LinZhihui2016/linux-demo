b22c8eba8d3d0f4003328cd4ebda851ae5ede211
叶子 <519403248@qq.com>
2021-04-26 09:24:35 +0800

    debug

--- task/bv.task.ts
+++ task/bv.task.ts
@@ -6,21 +6,24 @@ let errorTime = 0
 export const updateBv = async () => {
   const [, bv] = await $redis.getList(redisTask('video', 0)).shift()
   if (bv) {
-    await $redis.getHash(redisTask('video', 1)).calc(bv)
-    const [, time] = await $redis.getHash(redisTask('video', 1)).get(bv)
-    if ((time && +time <= 5)) {
-      const res = await bvAction.postAdd({ bv })
-      const { err } = res.json.body
-      if (err) {
-        errorTime++
-        if (errorTime >= 20) {
-          await sleep(1000 * 60 * 10)
+    const [, info] = await $redis.str.get(['bilibili', 'video', bv].join(':'))
+    if (!info) {
+      await $redis.getHash(redisTask('video', 1)).calc(bv)
+      const [, time] = await $redis.getHash(redisTask('video', 1)).get(bv)
+      if ((time && +time <= 5)) {
+        const res = await bvAction.postAdd({ bv })
+        const { err } = res.json.body
+        if (err) {
+          errorTime++
+          if (errorTime >= 20) {
+            await sleep(1000 * 60 * 10)
+          }
+        } else {
+          await $redis.getHash(redisTask('video', 1)).del(bv)
         }
-      } else {
-        await $redis.getHash(redisTask('video', 1)).del(bv)
       }
+      await sleep(2000)
     }
-    await sleep(2000)
     await updateBv()
   }
 }
