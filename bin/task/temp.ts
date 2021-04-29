73ab40980fba9c2aecb6fb52368cf5459d1d3946
叶子 <519403248@qq.com>
2021-04-29 10:54:59 +0800

    结构更改

--- bin/task/temp.ts
+++ bin/task/temp.ts
@@ -1,7 +1,13 @@
-import { checkUp } from "../../modules/up/helper";
+import { $redis } from "../../tools/redis";
+import { sleep } from "../../util";
 
 const temp = async () => {
-  await checkUp()
+  const test = $redis.getList('test')
+  for (let i = 0; i < 100; i++) {
+    const [, v] = await test.shift();
+    await $redis.getHash('test2').calc(v)
+    await sleep(Math.ceil(Math.random() * 100))
+  }
 }
 temp().then(() => process.exit(1))
 
