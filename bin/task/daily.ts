eb154c3bdcb428bfb307d57f5fd424d3e9c015d4
叶子 <519403248@qq.com>
2021-04-29 11:47:45 +0800

    结构更改

--- bin/task/daily.ts
+++ bin/task/daily.ts
@@ -1,11 +1,15 @@
 import { rankDailyTask } from "../../modules/rank/task";
 import { checkVideo } from "../../modules/video/helper";
 import { checkUp } from "../../modules/up/helper";
+import { upLogTask } from "../../modules/up_log/task";
+import { videoLogTask } from "../../modules/video_log/task";
 
 const daily = async () => {
   await rankDailyTask()
   await checkVideo()
   await checkUp()
+  await upLogTask()
+  await videoLogTask()
 }
 
 daily().then(() => process.exit())
