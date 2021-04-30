eadef63f8dbf8a6cd5dc350bd8b0ff01b71544ef
叶子 <519403248@qq.com>
2021-04-30 10:06:40 +0800

    结构更改

--- modules/rank/task.ts
+++ modules/rank/task.ts
@@ -2,6 +2,7 @@ import { RankId } from "../../crawler/ranking";
 import { PRes } from "../../type";
 import { apiLog } from "../../util/log";
 import { postUpdate } from "./api";
+import { sleep } from "../../util";
 
 const helper = async (rid: RankId, retry = 0): PRes<string> => {
   const res = await postUpdate({ noCache: false, rid: rid as unknown as RankId })
@@ -18,6 +19,7 @@ const helper = async (rid: RankId, retry = 0): PRes<string> => {
 export const rankDailyTask = async () => {
   const rankIdList = Object.keys(RankId).filter(i => /^[0-9]*$/.test(i))
   for (const rid of rankIdList) {
+    await sleep(2000)
     await helper(rid as unknown as RankId)
   }
 }
