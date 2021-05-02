0395deccb0fe2d870d803ce3378e268c3d2eb2da
叶子 <519403248@qq.com>
2021-05-03 00:22:48 +0800

    /sql/count

--- bin/task/temp.ts
+++ bin/task/temp.ts
@@ -3,7 +3,6 @@ import { getPaging } from "../../modules/rank/api";
 import { RankId } from "../../crawler/ranking";
 
 const temp = async () => {
-  const a = await getPaging({ rid: RankId.全站, date: '2021-05-01' })
-  console.log(a)
+   await getPaging({ rid: RankId.全站, date: '2021-05-01' })
 }
 scriptStart(temp)
