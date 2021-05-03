ce0838d58bfd2a6e85b27958f2904e7e8ca408de
叶子 <519403248@qq.com>
2021-05-03 22:21:32 +0800

    debug

--- modules/sql/api.ts
+++ modules/sql/api.ts
@@ -39,7 +39,6 @@ export const getCreatedInWeek: Action = async () => {
   const video = await videoCreatedCount(start, end)
   if (up[0]) return error(ErrBase.mysql读取失败, up[0].message)
   if (video[0]) return error(ErrBase.mysql读取失败, video[0].message)
-  console.log(up, video)
   const $up = up[1]
   const $video = video[1]
   const map = new Map<string, { up: number, video: number }>()
