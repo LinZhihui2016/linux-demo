7c35607a5e7fa15a800259fcf2c6ae27f33b7e6e
叶子 <519403248@qq.com>
2021-05-04 12:09:27 +0800

    count

--- bin/task/temp.ts
+++ bin/task/temp.ts
@@ -1,9 +1,5 @@
 import { scriptStart } from "../../helper";
-import { getCountByDate } from "../../modules/up/mysql";
-import dayjs from "dayjs";
 
 const temp = async () => {
-  const res = await getCountByDate(dayjs(), dayjs().add(1, 'day'))
-  console.log(res)
 }
 scriptStart(temp)
